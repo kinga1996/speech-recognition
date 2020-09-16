eda6b9cfe70b0206d96c524d8000fdafb545a4b8
Kinga Kalisz <kingakalisz@gmail.com>
2020-09-16 14:19:41 +0200

    firsti commit

---
+++ script.js
@@ -0,0 +1,24 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const tranciptionsDiv = document.querySelector('.transcription');
+tranciptionsDiv.appendChild(p);
+
+recognition.addEventListener('result', e=>{
+	const transcipts = Array.from(e.results)
+		.map(result => result[0])
+		.map(result => result.transcript)
+		.join('');
+
+	if (e.results[0].isFinal) {
+    	p = document.createElement('p');
+    	tranciptionsDiv.appendChild(p);
+    }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
